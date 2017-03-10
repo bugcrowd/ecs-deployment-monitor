812686ba3a08f422646a542048154a47d82e136f
Coen Hyde <coen.hyde@gmail.com>
2017-03-09 17:18:49 -0800

    inital flesh out

---
+++ test/status-stream.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var statusStream = require('../lib/status-stream');
+
+describe('Status Stream', function() {
+  it('should return the status of a deployment in progress', function(done) {
+
+  });
+});
