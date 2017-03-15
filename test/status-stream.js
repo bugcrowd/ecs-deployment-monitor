466f8354252127393fc346ed2a27e09ced01a5e2
Coen Hyde <coen.hyde@gmail.com>
2017-03-15 15:08:20 -0700

    adding first test of status stream

--- test/status-stream.js
+++ test/status-stream.js
@@ -1,9 +1,20 @@
 'use strict'
 
-var statusStream = require('../lib/status-stream');
+var expect = require('expect.js');
+var AWS = require('aws-sdk-mock');
+
+var StatusStream = require('../lib/status-stream');
 
 describe('Status Stream', function() {
-  it('should return the status of a deployment in progress', function(done) {
+  it('should return call describeServices with correct params', function(done) {
+    AWS.mock('ECS', 'describeServices', function (params, cb){
+      expect(params.cluster).to.equal('cluster-yo');
+      expect(params.services).to.eql(['service-yo']);
+      done();
+    });
 
+    var stream = new StatusStream({cluster: 'cluster-yo', service: 'service-yo'});
+    stream._read();
   });
+
 });
