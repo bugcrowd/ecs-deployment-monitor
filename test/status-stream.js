2ce21ae81c18eee5363ece84ab68432090de5920
Coen Hyde <coen.hyde@gmail.com>
2017-05-11 16:39:57 -0700

    add test to ensure correct deployment status is returned when deploy is usurped by another deploy

--- test/status-stream.js
+++ test/status-stream.js
@@ -25,10 +25,27 @@ describe('Status Stream', function() {
   });
 
   describe('Status Determination', function() {
-    it('should return DEPLOYMENT_NOT_FOUND if no deployment matches taskDefinition provided', function() {
+    it('should return DEPLOYMENT_NOT_FOUND if no deployment matches taskDefinition provided', function(done) {
       var stream = new StatusStream({cluster: 'cluster-yo', service: 'service-yo', taskDefinition: 'wrong'});
+      var result = stream.determineStatus(fixtures['newDeployment']['services'][0]);
+      stream.destroy();
 
-      stream.determineStatus(fixtures['newDeployment']['services'][0]);
-    })
+      expect(result.status).to.equal('DEPLOYMENT_NOT_FOUND');
+      done();
+    });
+
+    it('should return DEPLOY_USURPED the deployment is no longer the primary deployment', function(done) {
+      var stream = new StatusStream({
+        cluster: 'cluster-yo',
+        service: 'service-yo',
+        taskDefinition: 'arn:aws:ecs:us-east-1:123456789012:task-definition/app:111'
+      });
+      
+      var result = stream.determineStatus(fixtures['usurpedDeployment']['services'][0]);
+      stream.destroy();
+
+      expect(result.status).to.equal('DEPLOY_USURPED');
+      done();
+    });
   });
 });
