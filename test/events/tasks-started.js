6ffca3a1ede09fb9520e7816ec43b7f5ab139285
Coen Hyde <coen.hyde@gmail.com>
2017-06-23 15:56:56 +1000

    remove task resource loading, now tested by parent Event class test

--- test/events/tasks-started.js
+++ test/events/tasks-started.js
@@ -17,26 +17,4 @@ describe('Events:TasksStartedEvent', function() {
   it('should detect a TasksStartedEvent event', function() {
     expect(TasksStartedEvent.test(rawEvent)).to.be.true;
   });
-
-  it('should construct TasksStartedEvent object from event', function(done) {
-    AWS.mock('ECS', 'describeTasks', function (params, cb){
-      expect(params).to.eql({
-        cluster: "cluster-yo",
-        tasks: [
-          '8ddca76b-3ca5-4166-9cbb-aebdb8631861',
-          '630213ae-e078-491e-8b67-4d3e2d1ce6fd',
-          'ec6a21c0-1f75-49fc-bbcd-144e77bd97b0',
-          'f61d518f-ef95-40a8-9ce9-afd6e1ada4a7'
-        ]
-      });
-
-      done(null, [ 1, 2, 3, 4 ]);
-    });
-
-    TasksStartedEvent.convert({ options: { clusterArn: 'cluster-yo' }}, rawEvent, (err, event) => {
-      expect(err).to.equal(null);
-      expect(event.tasks.length).to.equal(4);
-      done();
-    });
-  });
 });
