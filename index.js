9f158facaf2ee57ec9e5eade97de5a480f3bfed2
Coen Hyde <coen.hyde@gmail.com>
2017-06-23 14:42:53 +1000

    fix bugs with service poller

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const combiner = require('stream-combiner');
 const AWS = require('aws-sdk');
 
 const FormatConsoleTransformStream = require('./lib/format-console-transform-stream');
-const StatusStream = require('./lib/status-stream');
 
 module.exports = function(options) {
   AWS.config.update({
