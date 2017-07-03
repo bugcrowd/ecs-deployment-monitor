a463d3a4431e610b74e80fd728010d81720b7e84
Coen Hyde <coen.hyde@gmail.com>
2017-07-03 15:21:56 +1000

    adding usurped evaluator

---
+++ test/evaluators/usurped.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const expect = require('expect.js');
+const EventEmitter = require('events');
+
+const helpers = require('../helpers');
+const fixtures = require('../fixtures');
+
+var Deployment = require('../../lib/deployment');
+var evaluators = require('../../lib/evaluators');
+
+describe('Evaluator:Usurped', function() {
+  var evaluator = evaluators['Usurped'];
+
+  it('should return FALSE when focused deployment is the primary deployment', function(done) {
+    var service = new EventEmitter();
+    service.initiated = true;
+    service.raw = fixtures['newDeployment']['services'][0];
+
+    var deployment = new Deployment({service: service, taskDefinitionArn: service.raw.taskDefinition});
+    evaluator(deployment, (err, result) => {
+      expect(result).to.equal(false);
+      deployment.destroy();
+      done();
+    });
+  });
+
+  it('should return TRUE when focused deployment is NOT the primary deployment', function(done) {
+    var service = new EventEmitter();
+    service.initiated = true;
+    service.raw = fixtures['newDeployment']['services'][0];
+
+    var deployment = new Deployment({service: service, taskDefinitionArn: 'arn:aws:ecs:us-east-1:123456789012:task-definition/app:110'});
+    evaluator(deployment, (err, result) => {
+      expect(result).to.equal(true);
+      deployment.destroy();
+      done();
+    });
+  });
+});
