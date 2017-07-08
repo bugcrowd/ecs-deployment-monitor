94fa86f0bef8a106343cf1e2665ac08c732a6867
Coen Hyde <coen.hyde@gmail.com>
2017-07-08 21:41:13 +1000

    adding live evaluator

---
+++ test/evaluators/live.js
@@ -0,0 +1,74 @@
+'use strict'
+
+const expect = require('expect.js');
+const EventEmitter = require('events');
+const sinon = require('sinon');
+
+const helpers = require('../helpers');
+const fixtures = require('../fixtures');
+
+var Deployment = require('../../lib/deployment');
+var evaluators = require('../../lib/evaluators');
+
+describe('Evaluator:Live', function() {
+  var evaluator = evaluators['Live'];
+
+  it('should return FALSE when there are no active tasks', function(done) {
+    var service = new EventEmitter();
+    service.initiated = true;
+    service.raw = fixtures['newDeployment']['services'][0];
+
+    var deployment = new Deployment({service: service, taskDefinitionArn: service.raw.taskDefinition});
+
+    evaluator(deployment, (err, result) => {
+      expect(result).to.equal(false);
+      deployment.destroy();
+      done();
+    });
+  });
+
+  it('should return FALSE when active tasks are not healthy', function(done) {
+    var service = new EventEmitter();
+    service.initiated = true;
+    service.raw = fixtures['newDeployment']['services'][0];
+
+    var isTaskHealthyStub = sinon
+      .stub(Deployment.prototype, "isTaskHealthy")
+      .callsFake(() => false);
+
+    var deployment = new Deployment({service: service, taskDefinitionArn: service.raw.taskDefinition});
+    deployment.tasksStarted = ["arn::task:1", "arn::task:2"];
+
+    evaluator(deployment, (err, result) => {
+      expect(result).to.equal(false);
+      deployment.destroy();
+      isTaskHealthyStub.restore();
+      done();
+    });
+  });
+
+  it('should return TRUE when number of healthy tasks are equal to or greater than the desired count', function(done) {
+    var service = new EventEmitter();
+    service.initiated = true;
+    service.raw = fixtures['newDeployment']['services'][0];
+
+    var isTaskHealthyStub = sinon
+      .stub(Deployment.prototype, "isTaskHealthy")
+      .callsFake(() => true);
+
+    var deployment = new Deployment({service: service, taskDefinitionArn: service.raw.taskDefinition});
+    deployment.tasksStarted = [
+      "arn::task:1",
+      "arn::task:2",
+      "arn::task:3",
+      "arn::task:4"
+    ];
+
+    evaluator(deployment, (err, result) => {
+      expect(result).to.equal(true);
+      deployment.destroy();
+      isTaskHealthyStub.restore();
+      done();
+    });
+  });
+});
