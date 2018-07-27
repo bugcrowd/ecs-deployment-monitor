0f8256a93400b09ee33ea67c1b16132bbabd7cd7
Coen Hyde <coen.hyde@gmail.com>
2018-07-27 18:04:56 -0500

    set default aws region to avoid tests failing when no aws configuration is available

--- test/helpers.js
+++ test/helpers.js
@@ -3,8 +3,15 @@
 const path = require('path');
 const AWS = require('aws-sdk-mock');
 
+// Set the default region to 'us-east-1' if not already set
+if (!AWS.config.region) {
+  AWS.config.update({
+    region: process.env.AWS_DEFAULT_REGION || 'us-east-1'
+  });
+}
+
 AWS.setSDK(path.resolve('node_modules/aws-sdk'));
 
 module.exports = {
-  afterEach: () => AWS.restore()
+  afterEach: () => AWS.restore();
 }
