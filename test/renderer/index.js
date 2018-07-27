a7ced20a7a9dbd87a15f6c6fb319fe018c9b322c
Coen Hyde <coen.hyde@gmail.com>
2018-07-27 16:24:01 -0500

    add test for renderer duration text

--- test/renderer/index.js
+++ test/renderer/index.js
@@ -3,12 +3,21 @@
 const expect = require('expect.js');
 const _ = require('lodash');
 const AWS = require('aws-sdk-mock');
+const moment = require('moment');
 const sinon = require('sinon');
+const tk = require('timekeeper');
 
 const helpers = require('../helpers');
+const Renderer = require('../../lib/renderer');
 
 describe('Renderer', function() {
   afterEach(helpers.afterEach);
 
+  it('should render duration text', function() {
+    let fakeDate = Date.now();
+    tk.freeze(fakeDate);
 
+    Renderer._setStateStartedAt(moment(fakeDate).subtract(3782, 'seconds'));
+    expect(Renderer._getStateDurationText()).to.equal('1 hours 3 minutes 2 seconds');
+  });
 });
